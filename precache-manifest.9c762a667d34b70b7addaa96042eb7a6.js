1337a767b2dece4cf011e2a2a8173a89180292ab
최종화 <cjhwa86@midassoft.co.kr>
2023-03-22 11:06:42 +0900

    Updates

---
+++ precache-manifest.9c762a667d34b70b7addaa96042eb7a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8367db2d7d386e18da2c",
+    "url": "/tesla-clone/static/css/main.8c45b61e.chunk.css"
+  },
+  {
+    "revision": "8367db2d7d386e18da2c",
+    "url": "/tesla-clone/static/js/main.8367db2d.chunk.js"
+  },
+  {
+    "revision": "40a913067904795b6e96",
+    "url": "/tesla-clone/static/js/1.40a91306.chunk.js"
+  },
+  {
+    "revision": "2552e2e649d02919a8c4",
+    "url": "/tesla-clone/static/js/2.2552e2e6.chunk.js"
+  },
+  {
+    "revision": "2ed78890656453c4b661",
+    "url": "/tesla-clone/static/js/runtime~main.2ed78890.js"
+  },
+  {
+    "revision": "52f6a767518922cb88c3ea5fc9b181c2",
+    "url": "/tesla-clone/index.html"
+  }
+];
